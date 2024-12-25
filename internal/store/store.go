d9314e6c1be160a1b52d1c36d8ea3e229f4313f2
moriT958 <mkdeadg@gmail.com>
2024-12-25 15:01:10 +0900

    create todo

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,1 +1,24 @@
 package store
+
+import "just-do-it-2/internal/model"
+
+var _ model.ITodoStore = (*TodoStore)(nil)
+
+type TodoStore struct {
+}
+
+func New() *TodoStore {
+	return &TodoStore{}
+}
+
+func (s *TodoStore) Save(todo model.Todo) {}
+
+func (s *TodoStore) GetAll(num int) []model.Todo {
+	return nil
+}
+func (s *TodoStore) FindByID(id string) model.Todo {
+	return model.Todo{}
+}
+func (s TodoStore) DeleteByID(id string) bool {
+	return false
+}
